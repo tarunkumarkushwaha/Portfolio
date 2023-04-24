14f4e68b8f74a664aae5ebdd23e113b985a5343d
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-04-24 23:20:21 +0530

    project card added

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
+// dropdown 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
@@ -14,4 +14,41 @@ window.onclick = function(event) {
       }
     }
   }
+}
+// scroll 
+
+
+
+// project slider 
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+const showproject = () => {
+  var elem = document.getElementsByClassName("slideshow-container")[0];
+  elem.scrollIntoView()
+}
+
+const showabout = () => {
+  var elem = document.getElementsByClassName("about")[0];
+  elem.scrollIntoView()
 }
