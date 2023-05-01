9d129a986cd807676763c640ff9b79804943dabc
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-05-01 15:03:39 +0530

    Basic layout

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-// dropdown 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+const showproject = () => {
+  var elem = document.getElementsByClassName("slideshow-container")[0];
+  elem.scrollIntoView()
 }
 
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+const showabout = () => {
+  var elem = document.getElementsByClassName("about")[0];
+  elem.scrollIntoView()
 }
-// scroll 
 
-
+const showcontact = () => {
+  var elem = document.getElementsByClassName("contact")[0];
+  elem.scrollIntoView()
+}
+const showresume = () => {
+  window.location= "https://github.com/tarunkumarkushwaha/Portfolio/raw/master/Downloads/Tarun Resume.pdf"
+}
 
 // project slider 
 
@@ -43,12 +40,42 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 }
 
-const showproject = () => {
-  var elem = document.getElementsByClassName("slideshow-container")[0];
-  elem.scrollIntoView()
+// testings
+
+// navbar active button 
+
+const aboutbtn = document.getElementsByClassName("aboutbtn")[0];
+const projectsbtn = document.getElementsByClassName("projectsbtn")[0];
+const contactbtn = document.getElementsByClassName("contactbtn")[0];
+const about = document.getElementsByClassName("about")[0].getBoundingClientRect();
+const projects = document.getElementsByClassName("projects")[0].getBoundingClientRect();
+const contact = document.getElementsByClassName("contact")[0].getBoundingClientRect();
+let scrolltop = document.documentElement.scrollTop
+let scrollheight = document.documentElement.scrollHeight
+let windowheight = window.innerHeight
+
+var width = screen.availWidth;
+var height = screen.availHeight;
+
+// scrollbar
+const test = () => {
+  const rect = about;
+  console.log(rect.top, rect.bottom);
+  let position = windowheight + scrolltop
+  console.log("position= ", position, "scrolltop = ", scrolltop, " scrollheight= ", scrollheight, " windowheight= ", windowheight)
 }
 
-const showabout = () => {
-  var elem = document.getElementsByClassName("about")[0];
-  elem.scrollIntoView()
+// progress 
+function update() {
+  var element = document.getElementById("myprogressBar");
+  var width = 1;
+  var identity = setInterval(scene, 10);
+  function scene() {
+    if (width >= 100) {
+      clearInterval(identity);
+    } else {
+      width++;
+      element.style.width = width + '%';
+    }
+  }
 }
