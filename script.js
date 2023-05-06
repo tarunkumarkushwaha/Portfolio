256053b790659cca0ab5fa5c6be9ffdb2953ea89
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-05-06 15:02:03 +0530

    features added

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const showcontact = () => {
   elem.scrollIntoView()
 }
 const showresume = () => {
-  window.location= "https://github.com/tarunkumarkushwaha/Portfolio/raw/master/Downloads/Tarun Resume.pdf"
+  window.location = "https://github.com/tarunkumarkushwaha/Portfolio/raw/master/Downloads/Tarun Resume.pdf"
 }
 
 // project slider 
@@ -40,42 +40,59 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 }
 
-// testings
+// auto slide scroll 
+
+setInterval(()=>{plusSlides(1)},3000)
 
 // navbar active button 
 
 const aboutbtn = document.getElementsByClassName("aboutbtn")[0];
 const projectsbtn = document.getElementsByClassName("projectsbtn")[0];
 const contactbtn = document.getElementsByClassName("contactbtn")[0];
-const about = document.getElementsByClassName("about")[0].getBoundingClientRect();
-const projects = document.getElementsByClassName("projects")[0].getBoundingClientRect();
-const contact = document.getElementsByClassName("contact")[0].getBoundingClientRect();
-let scrolltop = document.documentElement.scrollTop
-let scrollheight = document.documentElement.scrollHeight
-let windowheight = window.innerHeight
+const about = document.getElementsByClassName("about")[0];
+const projects = document.getElementsByClassName("projects")[0];
+const contact = document.getElementsByClassName("contact")[0];
 
-var width = screen.availWidth;
-var height = screen.availHeight;
-
-// scrollbar
-const test = () => {
-  const rect = about;
-  console.log(rect.top, rect.bottom);
-  let position = windowheight + scrolltop
-  console.log("position= ", position, "scrolltop = ", scrolltop, " scrollheight= ", scrollheight, " windowheight= ", windowheight)
-}
-
-// progress 
-function update() {
-  var element = document.getElementById("myprogressBar");
-  var width = 1;
-  var identity = setInterval(scene, 10);
-  function scene() {
-    if (width >= 100) {
-      clearInterval(identity);
-    } else {
-      width++;
-      element.style.width = width + '%';
-    }
+const scrolleffect = () => {
+  if (about.getBoundingClientRect().top <= 0 && projects.getBoundingClientRect().top > 0) {
+    aboutbtn.classList.add('on')
+    projectsbtn.classList.remove('on')
+  }
+  else if (projects.getBoundingClientRect().top <= 0 && contact.getBoundingClientRect().top > 0) {
+    projectsbtn.classList.add('on')
+    aboutbtn.classList.remove('on')
+    contactbtn.classList.remove('on')
+  }
+  else if (contact.getBoundingClientRect().top <= 0) {
+    contactbtn.classList.add('on')
+    projectsbtn.classList.remove('on')
+  }
+  else {
+    aboutbtn.classList.remove('on')
+    projectsbtn.classList.remove('on')
+    contactbtn.classList.remove('on')
   }
 }
+
+window.addEventListener('scroll',scrolleffect)
+
+// testings
+
+const test = ()=>{
+  console.log("test")
+}
+
+// progress
+// function update() {
+//   var element = document.getElementById("myprogressBar");
+//   var width = 1;
+//   var identity = setInterval(scene, 10);
+//   function scene() {
+//     if (width >= 100) {
+//       clearInterval(identity);
+//     } else {
+//       width++;
+//       element.style.width = width + '%';
+//     }
+//   }
+// }
